0d11ef050f8f2fbfe6233234c657dcb768811405
Henry Slawniak <meggawatts@gmail.com>
2013-08-15 19:39:39 -0500

    Update debug to ignore ui's snowball

--- make-debug.bat
+++ make-debug.bat
@@ -5,5 +5,5 @@ cd %oldDir%
 if not exist %newDir%\Loth-DEBUG mkdir %newDir%\Loth-DEBUG
 xcopy /y/s %oldDir%\src\main\meta\* %newDir%\Loth-DEBUG
 xcopy /y/s %oldDir%\src\main\pack\* %newDir%\Loth-DEBUG\assets\minecraft
-xcopy /y/s %oldDir%\MCME-UI\src\main\pack\*.* %newDir%\Loth-DEBUG\assets\minecraft
+xcopy /y/s %oldDir%\MCME-UI\src\main\pack\*.* %newDir%\Loth-DEBUG\assets\minecraft /exclude:debug-excludes.txt
 pause
