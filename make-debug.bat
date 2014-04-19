23eb0e1900904c9a16ad159bafa77281723f211a
Henry Slawniak <meggawatts@gmail.com>
2014-04-19 13:34:36 -0500

    Update debug script

--- make-debug.bat
+++ make-debug.bat
@@ -2,10 +2,9 @@ SET oldDir=%CD%
 cd %appdata%\.minecraft\resourcepacks
 SET newDir=%CD%
 cd %oldDir%
-if not exist %newDir%\Loth-DEBUG mkdir %newDir%\Loth-DEBUG
-del %newdir%\Loth-DEBUG\*.* /s /q
-xcopy /y/s %oldDir%\src\main\meta\* %newDir%\Loth-DEBUG
-xcopy /y/s %oldDir%\src\main\pack\* %newDir%\Loth-DEBUG\assets\minecraft
-xcopy /y/s %oldDir%\MCME-UI\src\main\pack\*.* %newDir%\Loth-DEBUG\assets\minecraft /exclude:debug-excludes.txt
-xcopy /y/s %oldDir%\MCME-Aural-Experience\src\main\pack\*.* %newDir%\Loth-DEBUG\assets\minecraft
-pause
+if not exist %newDir%\MCME-Loth-DEBUG mkdir %newDir%\MCME-Loth-DEBUG
+del %newdir%\MCME-Loth-DEBUG\*.* /s /q
+xcopy /y/s %oldDir%\src\main\meta\* %newDir%\MCME-Loth-DEBUG
+xcopy /y/s %oldDir%\src\main\pack\* %newDir%\MCME-Loth-DEBUG\assets\minecraft
+xcopy /y/s %oldDir%\MCME-UI\src\main\pack\*.* %newDir%\MCME-Loth-DEBUG\assets\minecraft /exclude:debug-excludes.txt
+xcopy /y/s %oldDir%\MCME-Aural-Experience\src\main\pack\*.* %newDir%\MCME-Loth-DEBUG\assets\minecraft
